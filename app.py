95486f8fe85ffa6995b58d330615006a3713ee26
neg9414 <neg9414@edu.neg.dk>
2025-09-30 09:43:40 +0200

    søging-W

--- app.py
+++ app.py
@@ -2,13 +2,7 @@ from flask import Flask, request, render_template, g
 import sqlite3
 
 app = Flask(__name__)
-DB_FILE = "./db/genes.db"
-
-def hent_db_genes():
-    if "db" not in g:
-        g.db = sqlite3.connect("genes.sql")
-        g.db.row_factory = sqlite3.Row
-    return g.db
+DB_FILE = "C:/Users/Lenovo/OneDrive - EUC Nordvestsjælland/1. Mine ting/Programmering B/Python/Database_wepservere/artworks.db"
 
 @app.route("/")
 def forside():
@@ -18,6 +12,18 @@ def forside():
 def login_profil():
     return render_template("login_profil.html")
 
+def hent_db_genes():
+    if "db" not in g:
+        g.db = sqlite3.connect("artworks.db")
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop("db", None)
+    if db is not None:
+         db.close()
+
 @app.route("/søgeside", methods=["GET", "POST"])
 def søgeside():
         if request.method == "POST":
@@ -25,12 +31,14 @@ def søgeside():
             return søg_i_db(søgeord)
         return render_template("søgeside.html", titel="Velkommen", resultater=[])
 
+#id, artist, painting, year, genre, image
 def søg_i_db(søgeord):
     db = hent_db_genes() 
-    cur = db.execute("""SELECT id, gene_name, gene_family, description FROM genes WHERE gene_name LIKE ? OR description LIKE ?""", ('%' + søgeord + '%'))
+    cur = db.execute("SELECT painting, artist FROM artworks WHERE artist LIKE ? ", ('%' + søgeord + '%',))
     data = cur.fetchall()
-    resultater = {"resultater": [dict(u) for u in data]}
-    return render_template("sødeside.html", titel="Søgeresultater", resultater=resultater)
+    resultater = {"resultater": [dict(r) for r in data]}
+    print(resultater)
+    return render_template("søgeside.html", titel="Søgeresultater", resultater=resultater)
 
 @app.route("/docs")
 def docs():
